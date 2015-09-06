40e548ab909bd51051cfaed929e6a3e1d0b9ad67
duxiaodong <dxdfive@gmail.com>
2015-09-06 21:27:24 +0800

    optimize loading

--- src/app/services/loading.service.js
+++ src/app/services/loading.service.js
@@ -1,12 +1,18 @@
 'use strict';
 
 angular.module('app')
-  .service('xdLoading', function($rootScope) {
+  .service('xdLoading', function($rootScope, $timeout) {
+    var timeout = null;
     this.show = function() {
+      if (timeout) {
+        $timeout.cancel(timeout);
+      }
       $rootScope.$broadcast('loading.show');
     };
     this.hide = function() {
-      $rootScope.$broadcast('loading.hide');
+      $timeout(function() {
+        $rootScope.$broadcast('loading.hide');
+      }, 500);
     };
   })
 ;
