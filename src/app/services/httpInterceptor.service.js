9ab80e932bb6768af770ec0de923d71054ebad47
duxiaodong <duxiaodong@darlin.me>
2015-11-25 18:44:34 +0800

    fix bug

--- src/app/services/httpInterceptor.service.js
+++ src/app/services/httpInterceptor.service.js
@@ -25,7 +25,7 @@ angular.module('app')
         xdLoading.hide();
         var text = rejection.statusText || errorText;
         xdAlert.show(text);
-        return rejection;
+        return $q.reject(rejection);
       }
     };
     return interceptor;
